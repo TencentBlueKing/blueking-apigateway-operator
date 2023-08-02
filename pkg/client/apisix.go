6333638d282c9cfb5a90c4fe826032b2c025b83d
Han-Ya-Jun <1581532052@qq.com>
2023-08-02 22:15:56 +0800

    fix: fix linter

--- pkg/client/apisix.go
+++ pkg/client/apisix.go
@@ -19,25 +19,25 @@
 package client
 
 import (
-    "fmt"
-    "io"
-    "net/http"
-    "sync"
-    "time"
+	"fmt"
+	"io"
+	"net/http"
+	"sync"
+	"time"
 
-    json "github.com/json-iterator/go"
-    "github.com/spf13/cast"
-    "gopkg.in/eapache/go-resiliency.v1/retrier"
-    retry "gopkg.in/h2non/gentleman-retry.v2"
-    "gopkg.in/h2non/gentleman.v2"
-    "gopkg.in/h2non/gentleman.v2/plugins/url"
+	json "github.com/json-iterator/go"
+	"github.com/spf13/cast"
+	"gopkg.in/eapache/go-resiliency.v1/retrier"
+	retry "gopkg.in/h2non/gentleman-retry.v2"
+	"gopkg.in/h2non/gentleman.v2"
+	"gopkg.in/h2non/gentleman.v2/plugins/url"
 
-    "github.com/TencentBlueKing/blueking-apigateway-operator/pkg/config"
-    "github.com/TencentBlueKing/blueking-apigateway-operator/pkg/logging"
+	"github.com/TencentBlueKing/blueking-apigateway-operator/pkg/config"
+	"github.com/TencentBlueKing/blueking-apigateway-operator/pkg/logging"
 )
 
 const (
-    getPublishVersionURL = "/api/:gateway/:stage/_version"
+	getPublishVersionURL = "/api/:gateway/:stage/_version"
 )
 
 var apisixClient *ApisixClient
@@ -45,107 +45,107 @@ var apisixClient *ApisixClient
 var apisxiOnce sync.Once
 
 type ApisixClient struct {
-    baseClient
-    // apisix版本探测次数
-    versionProbeCount int
-    // apisix版本探测间隔
-    versionProbeInterval time.Duration
+	baseClient
+	// apisix版本探测次数
+	versionProbeCount int
+	// apisix版本探测间隔
+	versionProbeInterval time.Duration
 }
 
 // InitApisixClient init apisix cli
 func InitApisixClient(cfg *config.Config) {
-    apisxiOnce.Do(func() {
-        cli := gentleman.New()
-        cli.URL(cfg.EventReporter.ApisixHost)
-        apisixClient = &ApisixClient{
-            baseClient:           baseClient{client: cli},
-            versionProbeCount:    cfg.EventReporter.VersionProbe.Retry.Count,
-            versionProbeInterval: cfg.EventReporter.VersionProbe.Retry.Interval,
-        }
-    })
+	apisxiOnce.Do(func() {
+		cli := gentleman.New()
+		cli.URL(cfg.EventReporter.ApisixHost)
+		apisixClient = &ApisixClient{
+			baseClient:           baseClient{client: cli},
+			versionProbeCount:    cfg.EventReporter.VersionProbe.Retry.Count,
+			versionProbeInterval: cfg.EventReporter.VersionProbe.Retry.Interval,
+		}
+	})
 }
 
 func GetApisixClient() *ApisixClient {
-    return apisixClient
+	return apisixClient
 }
 
 // GetReleaseVersion get apisix release info
 func (a *ApisixClient) GetReleaseVersion(gatewayName string, stageName string,
-    publishID string) (*VersionRouteResp, error) {
-    request := a.client.Request()
-    request.Path(getPublishVersionURL)
-    request.Method(http.MethodGet)
-    request.Use(url.Param("gateway", gatewayName))
-    request.Use(url.Param("stage", stageName))
-    retryStrategy := retrier.New(retrier.ConstantBackoff(
-        a.versionProbeCount, a.versionProbeInterval), nil)
-    var resp VersionRouteResp
-    retryError := new(error)
-    // set retry strategy
-    retry.Evaluator = retryEvaluator(gatewayName, stageName, cast.ToInt64(publishID), retryError, &resp)
-    retryPlugin := retry.New(retryStrategy)
-    request.Use(retryPlugin)
-    _, err := request.Send()
-    if err != nil {
-        return nil, err
-    }
-    if *retryError != nil {
-        return &resp, *retryError
-    }
+	publishID string) (*VersionRouteResp, error) {
+	request := a.client.Request()
+	request.Path(getPublishVersionURL)
+	request.Method(http.MethodGet)
+	request.Use(url.Param("gateway", gatewayName))
+	request.Use(url.Param("stage", stageName))
+	retryStrategy := retrier.New(retrier.ConstantBackoff(
+		a.versionProbeCount, a.versionProbeInterval), nil)
+	var resp VersionRouteResp
+	retryError := new(error)
+	// set retry strategy
+	retry.Evaluator = retryEvaluator(gatewayName, stageName, cast.ToInt64(publishID), retryError, &resp)
+	retryPlugin := retry.New(retryStrategy)
+	request.Use(retryPlugin)
+	_, err := request.Send()
+	if err != nil {
+		return nil, err
+	}
+	if *retryError != nil {
+		return &resp, *retryError
+	}
 
-    return &resp, nil
+	return &resp, nil
 }
 
 // retryEvaluator retry strategy
 func retryEvaluator(gateway string, stage string, publishID int64, retryError *error,
-    resp *VersionRouteResp) retry.EvalFunc {
-    return func(err error, res *http.Response, req *http.Request) error {
-        if err != nil {
-            return err
-        }
-        if res.StatusCode >= http.StatusInternalServerError || res.StatusCode == http.StatusTooManyRequests {
-            return retry.ErrServer
-        }
-        // 虚拟路由不存在,继续重试
-        if res.StatusCode == http.StatusNotFound {
-            *retryError = fmt.Errorf(
-                "configuration [gateway: %s,stage: %s] version route not found", gateway, stage)
-            logging.GetLogger().Info(*retryError)
-            return *retryError
-        }
-        if res.StatusCode == http.StatusOK {
-            // 解析返回结果
-            defer res.Body.Close()
-            result, readErr := io.ReadAll(res.Body)
-            if readErr != nil {
-                *retryError = fmt.Errorf("read configuration [gateway: %s,state: %s] version route body err: %w",
-                    gateway, stage, readErr)
-                logging.GetLogger().Error(*retryError)
-                return *retryError
-            }
-            unmarshalErr := json.Unmarshal(result, &resp)
-            if unmarshalErr != nil {
-                *retryError = fmt.Errorf(
-                    "unmarshal configuration [gateway: %s,stage: %s] version route body err: %w",
-                    gateway, stage, unmarshalErr)
-                logging.GetLogger().Error(*retryError)
-                return *retryError
-            }
-            // 判断版本号
-            if resp.PublishID < publishID {
-                // 如果获取到的版本号比当前小，说明当前的版本还未加载完成
-                *retryError = fmt.Errorf(
-                    "configuration [gateway: %s,stage: %s]  [current: %d, expected: %d] is not latest",
-                    gateway, stage, resp.PublishID, publishID)
-                logging.GetLogger().Info(*retryError)
-                return *retryError
-            }
-            // 如果发布的版本号比当前大，说明已经覆盖加载完成
-            if resp.PublishID >= publishID {
-                *retryError = nil
-                return nil
-            }
-        }
-        return nil
-    }
+	resp *VersionRouteResp) retry.EvalFunc {
+	return func(err error, res *http.Response, req *http.Request) error {
+		if err != nil {
+			return err
+		}
+		if res.StatusCode >= http.StatusInternalServerError || res.StatusCode == http.StatusTooManyRequests {
+			return retry.ErrServer
+		}
+		// 虚拟路由不存在,继续重试
+		if res.StatusCode == http.StatusNotFound {
+			*retryError = fmt.Errorf(
+				"configuration [gateway: %s,stage: %s] version route not found", gateway, stage)
+			logging.GetLogger().Info(*retryError)
+			return *retryError
+		}
+		if res.StatusCode == http.StatusOK {
+			// 解析返回结果
+			defer res.Body.Close()
+			result, readErr := io.ReadAll(res.Body)
+			if readErr != nil {
+				*retryError = fmt.Errorf("read configuration [gateway: %s,state: %s] version route body err: %w",
+					gateway, stage, readErr)
+				logging.GetLogger().Error(*retryError)
+				return *retryError
+			}
+			unmarshalErr := json.Unmarshal(result, &resp)
+			if unmarshalErr != nil {
+				*retryError = fmt.Errorf(
+					"unmarshal configuration [gateway: %s,stage: %s] version route body err: %w",
+					gateway, stage, unmarshalErr)
+				logging.GetLogger().Error(*retryError)
+				return *retryError
+			}
+			// 判断版本号
+			if resp.PublishID < publishID {
+				// 如果获取到的版本号比当前小，说明当前的版本还未加载完成
+				*retryError = fmt.Errorf(
+					"configuration [gateway: %s,stage: %s]  [current: %d, expected: %d] is not latest",
+					gateway, stage, resp.PublishID, publishID)
+				logging.GetLogger().Info(*retryError)
+				return *retryError
+			}
+			// 如果发布的版本号比当前大，说明已经覆盖加载完成
+			if resp.PublishID >= publishID {
+				*retryError = nil
+				return nil
+			}
+		}
+		return nil
+	}
 }
